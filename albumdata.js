35e7e578f16744b5ffe8700c5d0d4bf5aac72f8d
myjavhd1-beep <myjavhd1@gmail.com>
2026-06-12 21:53:41 +0530

    Update albumdata.js

--- albumdata.js
+++ albumdata.js
@@ -82,23 +82,33 @@ const albumData = [
     },
     { 
         id: 5, 
-        title: "Traditional Beauty", 
-        actress: "Yuna Ogura, Miru Sakamichi",
-        model: "Yuna Ogura", 
-        pages: 85, 
+        title: "The Fruit of Temptation Vol.1", 
+        actress: "Minamo",
+        pages: 18, 
         release: "2023-04-05",
-        publisher: "Heritage Media",
-        photographer: "Satoshi Kobayashi",
         tags: ["Traditional", "Kimono", "Elegant", "Cultural", "Japan"],
-        images: 5,
-        cover: "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-        albumImages: [
-            "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-            "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-            "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-            "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-        ]
-    },
+        images: 18,
+        cover:   "https://lh3.googleusercontent.com/d/1KSUdzcxHYfBvNHvB5b92sKuWpbyuQHjI",
+        albumImages: 
+[
+    "https://lh3.googleusercontent.com/d/1aPv9t-TfudTrcQza9wtkbE6P6YCKd5C7",
+    "https://lh3.googleusercontent.com/d/1-Gd5y21xE7SdFKSxNQRIVb4Z2ZuH9YuM",
+    "https://lh3.googleusercontent.com/d/1x3giArTYLEwmaCaIcoRENZ8pIaWLukzf",
+    "https://lh3.googleusercontent.com/d/1OFL1_evJdKroKxyKMEh4cvx9aDKuTe22",
+    "https://lh3.googleusercontent.com/d/174ScSbVOs8af2eycyr77_5rH1-81fgdk",
+    "https://lh3.googleusercontent.com/d/14GMcv-sbtfwPhpdIJEa9GXyGeglDqNmf",
+    "https://lh3.googleusercontent.com/d/1lptb3KOXALmOr7Jz8d3ZK1iPhWXDqxCz",
+    "https://lh3.googleusercontent.com/d/1JYRJjAzeApWeZw7_Ap93Y2khmTGPuBbZ",
+    "https://lh3.googleusercontent.com/d/1NGlqOMZ0OkduAelKAiN4Rah3y8bKnvqi",
+    "https://lh3.googleusercontent.com/d/19R3o1rr0n3qYoTwgqNp8390-VxC2xc_6",
+    "https://lh3.googleusercontent.com/d/1Xr86fjh96DJlbD_MWf7R9t_GrwNzSrzv",
+    "https://lh3.googleusercontent.com/d/13xSSEQNXcKemxci9vdEpSXxmJbBPKuS9",
+    "https://lh3.googleusercontent.com/d/1XlU3c0YW2imiwvNq2iKEGaPFfggBySJQ",
+    "https://lh3.googleusercontent.com/d/1Q-guCv-yUsXC5FqQixHcp9P9Kiehv6mP",
+    "https://lh3.googleusercontent.com/d/139ASG4XFYsSF-P9OzJNf1W--tHUmEWbU",
+    "https://lh3.googleusercontent.com/d/12WRxH4sG_lnMxNOcrWRFImNeqRWn6iqE",
+    "https://lh3.googleusercontent.com/d/1lhe6JCmadbAtz41lcY6wtzJv26TpLsnj"
+]    },
     { 
         id: 6, 
         title: "Mountain Escape", 
